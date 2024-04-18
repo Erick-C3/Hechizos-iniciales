3bdd386b2f530e665302c644905f501822c497f6
erick <>
2024-04-18 11:47:11 -0300

    ej3 secreto terminado

--- Hechizos nivel 1 - Secuencial/ej3-secreto.js
+++ Hechizos nivel 1 - Secuencial/ej3-secreto.js
@@ -14,6 +14,7 @@ const RACION_RECOMENDADA_DEF = 0;
 const ES_RACION_SUFICIENTE_DEF = false;
 
 const ESTADO_PERMISO_DEF = false;
+const ELECCION_ALIMENTAR_DEF = 0;
 function main() {
     let nombreHipogrifo = HIPOGRIFO_DEF_NOMBRE;
     let pesoHipogrifo = HIPOGRIFO_DEF_PESO;
@@ -22,8 +23,9 @@ function main() {
     let racionRecomendada = RACION_RECOMENDADA_DEF;
     let esRacionSuficiente = ES_RACION_SUFICIENTE_DEF;
     let estadoPermiso = ESTADO_PERMISO_DEF;
-
-    /* console.log("Como se llama el", ANIMAL_TIPO,"?");
+    let eleccionAlimentar = ELECCION_ALIMENTAR_DEF;
+    let estadoAtaque = false;
+    console.log("Como se llama el", ANIMAL_TIPO,"?");
     nombreHipogrifo = leer();
     hipogrifoEsSeguro = (nombreHipogrifo == HIPOGRIFO_SEGURO_1) || (nombreHipogrifo == HIPOGRIFO_SEGURO_2)
     console.log("Cual es el peso de", nombreHipogrifo);
@@ -32,13 +34,13 @@ function main() {
     racionDisponible = leer();
     racionRecomendada = RACION_HIPO_PROPORCION * pesoHipogrifo;
     esRacionSuficiente = racionDisponible >= racionRecomendada;
-    console.log("Tenes permiso de Hagrid para alimentar a hipogrifos?");
-    estadoPermiso = leer();
+    console.log("Tenes permiso de Hagrid para alimentar a hipogrifos?\n 1 para si / 0 para no");
+    estadoPermiso = Boolean(Number(leer()));
     esPosibleAlimentar = (estadoPermiso && esRacionSuficiente);
     esSeguro = (hipogrifoEsSeguro && esPosibleAlimentar)
- */
 
-   /*  console.log("Encontramos a: ", nombreHipogrifo);
+
+    console.log("Encontramos a: ", nombreHipogrifo);
     console.log("Es hipogrifo seguro: ", hipogrifoEsSeguro);
     console.log("Su peso aproximado:", pesoHipogrifo);
     console.log("La cantidad de comida disponible para alimentar:", racionDisponible);
@@ -46,9 +48,18 @@ function main() {
     console.log("Si es posible alimentarlo correctamente:", esRacionSuficiente);
     console.log("Si contas con el permiso de Hagrid:", estadoPermiso);
     console.log("Si podes alimentarlo:", esPosibleAlimentar);
-    console.log("Si es seguro:", esSeguro); */
+    console.log("Si es seguro:", esSeguro);
 
-    console.log(Math.random());
+    /* console.log(Math.random()); */
+    // codigo ascci del acento invertido - 96
+    console.log(`Queres alimentar a ${nombreHipogrifo}?\n 1 para si / 0 para no`);
+    eleccionAlimentar = Boolean(Number(leer()));
+    estadoAtaque = Math.random() <= POSIBILIDAD_ATAQUE;
+    resultFinalMision = (eleccionAlimentar && (esSeguro || !estadoAtaque)) ;
+
+    console.log("El estado final de la mision, termino bien?", resultFinalMision);
+    console.log("Ataco", estadoAtaque);
+
 }
 
 
