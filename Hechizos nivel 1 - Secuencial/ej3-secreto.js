edf6e85f77e47a17acd41b2f3b608252cf92d5a0
Erick <erick.nahuelpaiva.001@gmail.com>
2024-04-26 18:54:12 -0300

    ej terminado

--- Hechizos nivel 1 - Secuencial/ej3-secreto.js
+++ Hechizos nivel 1 - Secuencial/ej3-secreto.js
@@ -17,8 +17,11 @@ const DEF_POSIBLE_ALIMENTAR_CORRECTAMENTE = false;
 const DEF_PERMISO_DISP = false;
 const DEF_PUEDO_ALIMENTAR = false;
 
+const RESULTADO_NEGATIVO = false;
+const RESULTADO_POSITIVO = true;
+
 function main() {
-    let animalEncontrado = DEF_ANIMAL_ENCONTRADO;
+    let animalEncontradoNombre = DEF_ANIMAL_ENCONTRADO;
     let animalPeso = DEF_ANIMAL_PESO;
     let cantComidaDisp = DEF_CANT_COMIDA_DISP
     let cantComidaRecomendada = DEF_CANT_COMIDA_RECOMENDADA;
@@ -26,11 +29,14 @@ function main() {
     let tengoPermiso = DEF_PERMISO_DISP;
     let puedoAlimentarAlAnimal = DEF_PUEDO_ALIMENTAR;
     let esSeguroAlimentar = false;
+    let estadoResultadoFinal = RESULTADO_NEGATIVO;
+    let quiereAlimentar = false;
+    let estadoAtaque = false;
 
     console.log(`Como se llama el ${TIPO_ANIMAL} encontrado?`);
-    animalEncontrado = leer();
-    esAnimalAmigable = ((NOMBRE_ANIMAL_1 == animalEncontrado) || (NOMBRE_ANIMAL_2 == animalEncontrado));
-    console.log(`Cuanto pesa ${animalEncontrado}? en ${UNIDAD_PESO}`);
+    animalEncontradoNombre = leer();
+    esAnimalAmigable = ((NOMBRE_ANIMAL_1 == animalEncontradoNombre) || (NOMBRE_ANIMAL_2 == animalEncontradoNombre));
+    console.log(`Cuanto pesa ${animalEncontradoNombre}? en ${UNIDAD_PESO}`);
     animalPeso = leer();
     console.log(`Cuanta comida tenes disponible? en ${UNIDAD_PESO}`);
     cantComidaDisp = leer();
@@ -46,7 +52,7 @@ function main() {
     puedoAlimentarAlAnimal = ( tengoPermiso && suficienteComida);
     esSeguroAlimentar = (puedoAlimentarAlAnimal && esAnimalAmigable);
 
-    console.log(`Nombre del ${TIPO_ANIMAL} encontrado:`, animalEncontrado);
+    console.log(`Nombre del ${TIPO_ANIMAL} encontrado:`, animalEncontradoNombre);
     console.log(`Es un ${TIPO_ANIMAL} amigable?`, esAnimalAmigable);
     console.log(`Su peso es ${animalPeso}`, UNIDAD_PESO);
     console.log(`Cantidad de comida disponible para alimentar:`, cantComidaDisp);
@@ -56,7 +62,14 @@ function main() {
     console.log(`Podes alimentarlo?`, puedoAlimentarAlAnimal);
     console.log(`Es seguro alimentar al ${TIPO_ANIMAL}:`,esSeguroAlimentar);
 
-    console.log(Math.random());
+    /* console.log(Math.random()); */// 0-1(menor que <)
+    console.log(`Viendo informe, queres alimentar al ${TIPO_ANIMAL} ${animalEncontradoNombre}?  1 para si y 0 para no` );
+    quiereAlimentar = Boolean(Number(leer()));
+    // es true si no resulte herido, caso contrario false
+    estadoAtaque = Math.random() <= PROBABILIDAD_ATAQUE;
+    console.log("Resultado de ataque aleatorio", estadoAtaque);
+    estadoResultadoFinal = quiereAlimentar && (esSeguroAlimentar || !(estadoAtaque)) ;
+    console.log("Resultado final:", estadoResultadoFinal);
 }
 
 
